f6d7e892a109bba06801a0c35289aca882f93163
Stepan Obraztsov <stepobr@cern.ch>
2017-02-13 10:43:41 +0100

    some more updates during HE 2017 campaign

--- HCALSourceDataMonitor/hcalsourcedatamonplots.py
+++ HCALSourceDataMonitor/hcalsourcedatamonplots.py
@@ -4,8 +4,8 @@ process = cms.PSet()
 
 
 process.HCALSourceDataMonPlots = cms.PSet(
-    RootInputFileName = cms.string('264742.root'),
-    RootOutputFileName = cms.string('264742_plots.root'),
+    RootInputFileName = cms.string('ntuple_da_287084.root'),
+    RootOutputFileName = cms.string('plots_da_287084.root'),
     NewRowEvery = cms.int32(2),
     ThumbnailSize = cms.int32(350),
     OutputRawHistograms = cms.bool(False),
