063909c1d91c7c8ca35748f4b5cecdc2903bf3db
Stepan Obraztsov <stepobr@cern.ch>
2017-03-10 23:04:29 +0100

    some fixes for HF

--- HCALSourceDataMonitor/hcalsourcedatamonitor_cfg.py
+++ HCALSourceDataMonitor/hcalsourcedatamonitor_cfg.py
@@ -1,4 +1,10 @@
 import FWCore.ParameterSet.Config as cms
+                #erwarn['Non-existentent combination Eta/Phi/Layer'] += 1
+                #continue
+            
+            #if heDepths[mapKey] != depth:
+                #erwarn['Skipping data not associated with tube'] += 1
+                #continue
 
 process = cms.Process("HCALSourceDataMonitor")
 
@@ -9,14 +15,19 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 process.load('FWCore.Modules.printContent_cfi')
 
 process.load("Configuration.Geometry.GeometryIdeal_cff")
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = 'MCRUN2_74_V8A'
+#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+#process.GlobalTag.globaltag = 'MCRUN2_74_V8A'
+
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic', '')
+
 process.es_ascii = cms.ESSource('HcalTextCalibrations',
     input = cms.VPSet(
         cms.PSet(
             object = cms.string('ElectronicsMap'),
-            file = cms.FileInPath('HCALSourcing/HCALSourceDataMonitor/version_G_emap_HBHEuHTR.txt')
-            #file = cms.FileInPath('split_PMT_Box_UXC_emap.txt')
+            file = cms.FileInPath('HCALSourcing/HCALSourceDataMonitor/ngHF2017EMap_20170206_pre05.txt')
+            #file = cms.FileInPath('HCALSourcing/HCALSourceDataMonitor/emap_versionG_ngHF20170206_HEP17_CRF.txt')
             ),
 	)
     )
@@ -28,8 +39,9 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 # input files
 process.source = cms.Source("HcalTBSource",
     fileNames = cms.untracked.vstring(
- 	#'file:///data/sourcing/USC1702/USC_287084.root'
-  	'file:///data/sourcing/USC1702/USC_287083newmode.root'
+ 	#'file:///data/sourcing/USC1702/USC_288697.root'
+        'root://eoscms//eos/cms/store/group/dpg_hcal/comm_hcal/USC/run289112/USC_289112.root'
+        #'file:///data/sourcing/USC1702/USC_288690.root'
     )
 )
 
@@ -44,14 +56,14 @@ process.tbunpack = cms.EDProducer("HcalTBObjectUnpacker",
 process.histoUnpack = cms.EDProducer("HcalUTCAhistogramUnpacker",
           fedRawDataCollectionTag = cms.InputTag("source"),
           rawDump = cms.bool(False),
-	  fedNumber = cms.int32(64))
-#	  fedNumber = cms.int32(63))
+          fedNumber = cms.int32(60))
 
 # Tree-maker
 process.hcalSourceDataMon = cms.EDAnalyzer('HCALSourceDataMonitor',
-    RootFileName = cms.untracked.string('ntuple_da_287083.root'),
-    #RootFileName = cms.untracked.string('ntuple_da_287084.root'),
-    PrintRawHistograms = cms.untracked.bool(False),
+    #RootFileName = cms.untracked.string('/data/sourcing/Ntuples1702/ntuple_da_288924.root'),
+    RootFileName = cms.untracked.string('/data/sourcing/Ntuples1702/ntuple_da_289112.root'),
+    #PrintRawHistograms = cms.untracked.bool(False),
+    #SelectDigiBasedOnTubeName = cms.untracked.bool(True),
     SelectDigiBasedOnTubeName = cms.untracked.bool(True),
     HcalSourcePositionDataTag = cms.InputTag("tbunpack"),
     hcalTBTriggerDataTag = cms.InputTag("tbunpack"),
