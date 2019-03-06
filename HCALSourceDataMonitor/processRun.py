346160e8933e91c426eec40e7438028a7d7aad35
Stepan Obraztsov <stepobr@cern.ch>
2019-03-06 18:57:15 +0100

    update

--- HCALSourceDataMonitor/processRun.py
+++ HCALSourceDataMonitor/processRun.py
@@ -1,101 +1,89 @@
 #!/usr/bin/env python
+"""
+A script to process a single sourcing run
+# -*- coding: UTF-8 -*-
 
+"""
 import string
 import os
 import sys
 import subprocess
 
-dataDir = ''
+inputFileLocal = 'file:///data/sourcing/USC1702/USC___XXX__.root'
+inputFileEos = 'root://eoscms//eos/cms/store/group/dpg_hcal/comm_hcal/USC/run__XXX__/USC___XXX__.root'
+outFile = '/data/sourcing/Ntuples1903/ntuple_da___XXX__.root'
+
+cfgTemplate = 'hcalsourcedatamonitor_template_cfg.py'
+cfgFile = 'hcalsourcedatamonitor_cfg.py'
+
+anaPath = '/wwwlocal/p5s/Ana/Scripts/'
+script = './histoMakerHB.py'
+link = 'http://cmskam06.cern.ch/p5sourcing2019/profs.php'
+
+rawDataFile = ''
+ntupleFile = ''
 
 
-def GetNewCfgName(runNumber):
-  return 'hcalSourceDataMon.'+str(runNumber)+'_cfg.py'
+def makeCfgFile(rawDataFile, ntupleFile):
+    
+    f = open(cfgTemplate,'r')
+    tmpl = f.read()
+    f.close()
+    
+    tmpl = tmpl.replace('__XXX__', rawDataFile)
+    tmpl = tmpl.replace('__YYY__', ntupleFile)
+    
+    f = open(cfgFile, 'w')
+    f.write(tmpl)
+    f.close()
 
-def GetPlotCfgName(runNumber):
-  return 'hcalSourceDataMonPlots.'+str(runNumber)+'.py'
+def processRun():
+    if len(sys.argv) < 2:
+        print "Usage", sys.argv[0], "runNo [local]"
+        exit(1) 
+        
+    cwd = os.getcwd()
+    
+    runNo = str(sys.argv[1]).zfill(6)
+    if len(sys.argv) == 3 and sys.argv[2] == 'local':
+        #Check if file has been copied
+        rawDataFile = inputFileLocal.replace('__XXX__', runNo)
+        if not  os.path.isfile(rawDataFile.replace('file://', '')):
+            print rawDataFile,  'not found. Make sure that it\'s been copied from hcalutca01' 
+            exit(1)
+        print "Processing run", runNo, 'from local storage'
+    else:
+        #File from eos
+        rawDataFile = inputFileEos.replace('__XXX__', runNo)
+        print "Processing run", runNo, 'from eos'
+    
+    ntupleFile = outFile.replace('__XXX__', runNo)
+    makeCfgFile(rawDataFile, ntupleFile)
 
-def GetNewNTupleFileName(runNumber):
-  return 'hcalSourceDataMon.'+str(runNumber)+'.root'
-
-def GetPlotFileName(runNumber):
-  return 'hcalSourceDataMonPlots.'+str(runNumber)+'.root'
-
-def GetFilenames(runNumber):
-  global dataDir
-  fileNames = ''
-  oldDir = os.getcwd()
-  os.chdir(dataDir)
-  dirFileList = os.listdir(".")
-  dirFileList.sort()
-  dirFileList.sort(key=len) # put shortest file name (without .1, .2, etc., at beginning)
-  for files in dirFileList:
-    if files.endswith(".root") and str(runNumber) in files:
-      fileNames+=("      'file:"+dataDir+"/"+files+"',\n")
-  os.chdir(oldDir)
-  print 'using files:'
-  print fileNames
-  return fileNames
-
-
-def CreateNTupleConfigFile(runNumber):
-  Path_Cfg = 'hcalsourcedatamonitor_template_cfg.py'
-  newCfgName = GetNewCfgName(runNumber)
-  config_file=open(Path_Cfg,'r')
-  config_txt = config_file.read()
-  config_file.close()
-  # Replacements
-  config_txt = config_txt.replace("XXX_TFILENAME_XXX", GetNewNTupleFileName(runNumber))
-  config_txt = config_txt.replace("XXX_FILENAMES_XXX", GetFilenames(runNumber))
-  # write
-  config_file=open(str(runNumber)+'/'+newCfgName,'w')
-  config_file.write(config_txt)
-  config_file.close()
-
-
-def CreatePlotsConfigFile(runNumber):
-  Path_Cfg = 'hcalsourcedatamonplots_template.py'
-  newCfgName = GetPlotCfgName(runNumber)
-  config_file=open(Path_Cfg,'r')
-  config_txt = config_file.read()
-  config_file.close()
-  # Replacements
-  config_txt = config_txt.replace("XXX_NTUPLEFILE_XXX", GetNewNTupleFileName(runNumber))
-  config_txt = config_txt.replace("XXX_PLOTFILE_XXX", GetPlotFileName(runNumber))
-  # write
-  config_file=open(str(runNumber)+'/'+newCfgName,'w')
-  config_file.write(config_txt)
-  config_file.close()
-
-
-def CreateCfgs(runNumber):
-  if not os.path.isdir(str(runNumber)):
-    os.mkdir(str(runNumber))
-  CreateNTupleConfigFile(runNumber)
-  CreatePlotsConfigFile(runNumber)
-
-
-
-
-# RUN
-runsList = [
-    264662, 264663, 264680, 264695, 264700, 264707, 264708, 264710, 264742, 264746, 264750, 264768, 264785, 264787, 264789,
-]
-
-dataDir = "/afs/cern.ch/user/s/stepobr/eos/cms/store/group/dpg_hcal/comm_hcal/LS1"
-
-for run in runsList:
-  print 'Run number:',run
-  CreateCfgs(run)
-  print 'cmsRun',GetNewCfgName(run),'from:',os.getcwd()+'/'+str(run)
-  proc = subprocess.Popen(['cmsRun',GetNewCfgName(run)],cwd=os.getcwd()+'/'+str(run),stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-  out,err = proc.communicate()
-  print out
-  print err
-  proc = subprocess.Popen(['hcalSourceDataMonPlots',GetPlotCfgName(run)],cwd=os.getcwd()+'/'+str(run),stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-  out,err = proc.communicate()
-  print out
-  print err
-  
+    print "Running CMSSW for run", runNo, "..."
+    #proc = subprocess.Popen(['cmsRun', cfgFile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #out,err = proc.communicate()
+    #print out
+    #print err
+    command = 'cmsRun ' + cfgFile
+    os.system(command)
+    
+    
+    #print "Running ngHistoMaker for run", runNo, "..."
+    #os.chdir(anaPath)
+    #proc = subprocess.Popen([script, runNo], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #out,err = proc.communicate()
+    #print out
+    #print err
+    
+    os.chdir(cwd)
+    
+    print "=================================\n"
+    print "You can examine output at ", link, '\n'
+    
+#===========================================================
+if __name__ == "__main__":
+    processRun()
 
 
 
